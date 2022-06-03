82b6699d14d612cb086293a80df967b0863efcd0
Charles Abe <abecharles5@gmail.com>
2022-06-04 05:01:02 +0800

    Do feedback section

--- app/src/main/java/com/example/food_vision/activities/FeedbackPage.kt
+++ app/src/main/java/com/example/food_vision/activities/FeedbackPage.kt
@@ -2,28 +2,48 @@ package com.example.food_vision.activities
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.Toast
 import com.example.food_vision.R
+import com.example.food_vision.databinding.FeedbackpageBinding
+import java.net.URL
 
 class FeedbackPage : AppCompatActivity() {
+    lateinit var binding: FeedbackpageBinding
+
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.feedbackpage)
+        binding = FeedbackpageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val fbBack = findViewById<ImageView>(R.id.fbBack)
-        val fbSubmit = findViewById<ImageView>(R.id.fbBack)
-        val fbEmail = findViewById<EditText>(R.id.fbEmail)
-        val fbName = findViewById<EditText>(R.id.fbName)
-        val fbMulti = findViewById<EditText>(R.id.fbMulti)
-
-        fbBack.setOnClickListener{
+        binding.fbBack.setOnClickListener{
             val intent = Intent(this, Dashboard::class.java)
             startActivity(intent)
         }
 
+       binding.feedbackSubmit.setOnClickListener{
+            val email = "foodvision40@gmail.com"
+            val subject = "FoodVision application feedback"
+            val message = binding.fbMulti.text.toString()
+
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:")
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+            intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+
+            if(intent.resolveActivity(packageManager) != null){
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Required app is not installed", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
